58bf71d02f481e86da6f39412d1be4be730851ec
Luiz Arantes Sa <LArantesSa@icloud.com>
2017-11-05 08:50:27 -0600

    setup button outlets + actions

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var centerLabel: UILabel!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setDefaultState()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func pressedRecordButton(_ sender: UIButton) {
+        setRecordingState()
     }
-
-
+    
+    @IBAction func pressedStopButton(_ sender: UIButton) {
+        setDefaultState()
+    }
+    
+    func setDefaultState() {
+        recordButton.isEnabled = true
+        stopButton.isEnabled = false
+        centerLabel.text = "Tap to Record!"
+    }
+    
+    func setRecordingState() {
+        recordButton.isEnabled = false
+        stopButton.isEnabled = true
+        centerLabel.text = "Recording in Progress"
+    }
+    
 }
 
